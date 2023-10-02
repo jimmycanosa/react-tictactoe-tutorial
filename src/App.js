a0b7f2bf3318246af93722e119d7b91dc3711185
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2023-10-02 12:38:45 +0800

    Making interactive component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function Square({value}) {
-  return <button className='square'>{value}</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+  return (
+    <button className='square' onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
